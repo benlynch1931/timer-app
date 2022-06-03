cbbb36ecb5de88870be89ec54c51c2688a179712
Ben <ben.lynch1931@gmail.com>
2022-06-03 12:59:51 +0100

    Add javadocs to ComponentUtils

--- src/main/java/com/ar/utils/ComponentUtils.java
+++ src/main/java/com/ar/utils/ComponentUtils.java
@@ -15,20 +15,45 @@ public class ComponentUtils {
      */
     private ComponentUtils() {}
 
+    /**
+     * Returns twenty percent of a value
+     * @param value whole value
+     * @return 20% of whole value
+     */
     public static double twentyPct(final long value) {
         return value * 0.2;
     }
+
+    /**
+     * Returns thirty percent of a value
+     * @param value whole value
+     * @return 30% of whole value
+     */
     public static double thirtyPct(final long value) {
         return value * 0.3;
     }
+    /**
+     * Returns fifty percent of a value
+     * @param value whole value
+     * @return 50% of whole value
+     */
     public static double fiftyPct(final long value) {
         return value * 0.5;
     }
 
+    /**
+     * Sets Pane/container width and height
+     * @param container Pane to set dimensions
+     */
     public static void setTaskContainerSize(final Pane container) {
         setTaskContainerSize(container, ComponentSize.TASK_BOX_HGT);
     }
 
+    /**
+     * Sets Pane/container width and height
+     * @param container Pane to set dimensions
+     * @param height height of Pane
+     */
     public static void setTaskContainerSize(final Pane container, final double height) {
         container.setMaxWidth(ComponentSize.SCREEN_WIDTH);
         container.setMinWidth(ComponentSize.SCREEN_WIDTH);
@@ -36,6 +61,11 @@ public class ComponentUtils {
         container.setMinHeight(height);
     }
 
+    /**
+     * Sets Duration-specific TextField width, height and coordinates
+     * @param component Component to set dimensions
+     * @param fieldNo index representing order of HH->MM->SS
+     */
     public static void setDurationComponentDimensions(final TextField component, final int fieldNo) {
         component.setMaxWidth(ComponentSize.TASK_DURATION_FIELDS);
         component.setMaxWidth(ComponentSize.TASK_DURATION_FIELDS);
@@ -45,6 +75,10 @@ public class ComponentUtils {
         component.relocate(xCoordinate, ComponentSize.TASK_TEXT_FIELD_TOP);
     }
 
+    /**
+     * Sets Taskname-specific TextField width, height and coordinates
+     * @param component Component to set dimensions
+     */
     public static void setTaskNameComponentDimensions(final TextField component) {
         component.setMinHeight(ComponentSize.TASK_TEXT_FIELD_HGT);
         component.setMaxWidth(ComponentSize.TASK_NAME_FIELD_WIDTH);
@@ -54,6 +88,10 @@ public class ComponentUtils {
         component.relocate(0, ComponentSize.TASK_TEXT_FIELD_TOP);
     }
 
+    /**
+     * Sets taskview-specific buttons width, height and coordinates
+     * @param button Button to set dimensions
+     */
     public static void setTaskButtonDimensions(final Button button) {
         button.setMinWidth(ComponentSize.TASK_BTN_WIDTH);
         button.setMaxWidth(ComponentSize.TASK_BTN_WIDTH);
