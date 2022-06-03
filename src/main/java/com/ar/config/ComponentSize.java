9f5fdc4df1537588d98e6bde03c55d5eacd123a5
Ben <ben.lynch1931@gmail.com>
2022-06-03 12:54:02 +0100

    remove unused constants

--- src/main/java/com/ar/config/ComponentSize.java
+++ src/main/java/com/ar/config/ComponentSize.java
@@ -14,8 +14,7 @@ public class ComponentSize {
     public static final int TABLE_WIDTH = SCREEN_WIDTH - (2*TABLE_MARGIN);
     public static final int COL_PRESET_WIDTH = 210;
     public static final int COL_OTHER_WIDTH = (TABLE_WIDTH - COL_PRESET_WIDTH) / 2;
-
-    public static final int TASK_DURATION_TOP = (SCREEN_HEIGHT / 2) - 50;
+    
     public static final int TASK_DURATION_MARGIN = 69;
     public static final int TASK_DURATION_FIELDS = 69;
     public static final int TASK_DURATION_SPACER = 43;
@@ -27,9 +26,6 @@ public class ComponentSize {
     public static final double TASK_DURATION_PANE_HGT = TASK_BOX_HGT + TASK_TIME_ERROR_SIZE;
     public static final double TASK_NAME_BOX_TOP = 224;
     public static final double TASK_DURATION_PANE_TOP = 384;
-
-    public static final double TASK_NAME_BOX_TRANS_Y = TASK_NAME_BOX_TOP - TASK_BUTTON_BOX_TOP - TASK_BOX_HGT;
-    public static final double TASK_DURATION_PANE_TRANS_Y = TASK_DURATION_PANE_TOP - TASK_NAME_BOX_TOP - TASK_BOX_HGT;
 
     public static final int TASK_NAME_FIELD_WIDTH = SCREEN_WIDTH / 3 * 2;
     public static final int TASK_NAME_FIELD_MARGIN = TASK_NAME_FIELD_WIDTH / 4;
