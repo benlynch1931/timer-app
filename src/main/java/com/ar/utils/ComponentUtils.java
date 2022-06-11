af5bf634cc144621fb7f7a5ec97921d25b5ed55d
Ben <ben.lynch1931@gmail.com>
2022-06-11 12:42:49 +0100

    Remove unused methods in ComponentUtils

--- src/main/java/com/ar/utils/ComponentUtils.java
+++ src/main/java/com/ar/utils/ComponentUtils.java
@@ -48,102 +48,12 @@ public class ComponentUtils {
         return value * 0.5;
     }
 
-    /**
-     * Sets Pane/container width and height
-     * @param container Pane to set dimensions
-     */
-    public static void setTaskContainerSize(final Pane container) {
-        setTaskContainerSize(container, ComponentSize.TASK_BOX_HGT);
-    }
 
     /**
-     * Sets Pane/container width and height
-     * @param container Pane to set dimensions
-     * @param height height of Pane
+     * Sets Button-specific dimensions
+     * @param button button to set dimensions for
+     * @param dimensions dimensions values
      */
-    public static void setTaskContainerSize(final Pane container, final double height) {
-        container.setMaxWidth(ComponentSize.SCREEN_WIDTH);
-        container.setMinWidth(ComponentSize.SCREEN_WIDTH);
-        container.setMaxHeight(height);
-        container.setMinHeight(height);
-    }
-
-    /**
-     * Sets Duration-specific TextField width, height and coordinates
-     * @param component Component to set dimensions
-     * @param timeType enum representing order of HH->MM->SS
-     */
-    public static void setDurationComponentDimensions(final TextField component, final TimeType timeType) {
-//        component.setMaxWidth(ComponentSize.TASK_DURATION_FIELDS);
-//        component.setMaxWidth(ComponentSize.TASK_DURATION_FIELDS);
-        component.setMinHeight(ComponentSize.TASK_TEXT_FIELD_HGT);
-
-//        int xCoordinate = (ComponentSize.TASK_DURATION_MARGIN) + (ComponentSize.TASK_DURATION_FIELDS * timeType.times()) + (ComponentSize.TASK_DURATION_SPACER * timeType.times());
-//        component.relocate(xCoordinate, ComponentSize.TASK_TEXT_FIELD_TOP);
-    }
-
-    /**
-     * Sets TaskName-specific TextField width, height and coordinates
-     * @param component Component to set dimensions
-     */
-    public static void setTaskNameComponentDimensions(final TextField component) {
-        component.setMinHeight(ComponentSize.TASK_TEXT_FIELD_HGT);
-        component.setMaxWidth(ComponentSize.TASK_NAME_FIELD_WIDTH);
-        component.setMinWidth(ComponentSize.TASK_NAME_FIELD_WIDTH);
-        component.setTranslateX(ComponentSize.TASK_NAME_FIELD_MARGIN);
-
-        component.relocate(0, ComponentSize.TASK_TEXT_FIELD_TOP);
-    }
-
-    /**
-     * Sets taskView-specific buttons width, height and coordinates
-     * @param button Button to set dimensions
-     */
-    public static void setTaskButtonDimensions(final Button button) {
-        button.setMinWidth(ComponentSize.TASK_BTN_WIDTH);
-        button.setMaxWidth(ComponentSize.TASK_BTN_WIDTH);
-        button.setMaxHeight(ComponentSize.TASK_BTN_HEIGHT);
-        button.setMinHeight(ComponentSize.TASK_BTN_HEIGHT);
-        // Brings button to front of z-index
-        button.setViewOrder(-4);
-
-        if (button.getText().equals("SAVE")) {
-            button.relocate(ComponentSize.SCREEN_WIDTH - ComponentSize.TASK_BTN_WIDTH, ComponentSize.TASK_BTN_TOP);
-        } else {
-            button.relocate(0, ComponentSize.TASK_BTN_TOP);
-        }
-    }
-
-    /**
-     * Sets tasklistview-specific buttons width, height and coordinates
-     * @param button Button to set dimensions
-     */
-    public static void setTaskListButtonDimensions(final Button button) {
-        button.setMinWidth(ComponentSize.TASK_BTN_WIDTH);
-        button.setMaxWidth(ComponentSize.TASK_BTN_WIDTH);
-        button.setMaxHeight(ComponentSize.TASK_BTN_HEIGHT);
-        button.setMinHeight(ComponentSize.TASK_BTN_HEIGHT);
-        // Brings button to front of z-index
-        button.setViewOrder(-4);
-
-        if (button.getText().equals("EDIT")) {
-            button.relocate(ComponentSize.SCREEN_WIDTH - ComponentSize.TASK_BTN_WIDTH, ComponentSize.TASKLIST_BTN_TOP);
-        } else if (button.getText().equals("CLONE")) {
-            button.relocate(ComponentSize.SCREEN_WIDTH - ComponentSize.TASK_BTN_WIDTH, ComponentSize.TASKLIST_BTN_TOP + ComponentSize.TASK_BTN_HEIGHT + 5);
-        } else {
-            button.relocate(0, ComponentSize.TASKLIST_BTN_TOP);
-        }
-
-    }
-
-    public static <D> void setTableDimensions(final TableView<D> table) {
-        table.relocate(0, ComponentSize.LIST_TABLE_TOP);
-        table.setTranslateX(ComponentSize.TABLE_MARGIN);
-        table.setMaxWidth(ComponentSize.TABLE_WIDTH);
-        table.setMinWidth(ComponentSize.TABLE_WIDTH);
-        table.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
-    }
-
     public static void setButtonDimensions(final Button button, final Dimensions dimensions) {
         button.setMinWidth(dimensions.getWidth());
         button.setMaxWidth(dimensions.getWidth());
@@ -153,6 +63,11 @@ public class ComponentUtils {
         button.setViewOrder(dimensions.getZ());
     }
 
+    /**
+     * Sets TextField-specific dimensions
+     * @param field field to set dimensions for
+     * @param dimensions dimensions values
+     */
     public static void setTextFieldDimensions(final TextField field, final Dimensions dimensions) {
         field.setMinWidth(dimensions.getWidth());
         field.setMaxWidth(dimensions.getWidth());
@@ -162,6 +77,11 @@ public class ComponentUtils {
         field.setViewOrder(dimensions.getZ());
     }
 
+    /**
+     * Sets AnchorPane-specific dimensions
+     * @param pane pane to set dimensions for
+     * @param dimensions dimensions values
+     */
     public static void setPaneDimensions(final AnchorPane pane, final Dimensions dimensions) {
         pane.setMinWidth(dimensions.getWidth());
         pane.setMaxWidth(dimensions.getWidth());
@@ -171,6 +91,11 @@ public class ComponentUtils {
         pane.setViewOrder(dimensions.getZ());
     }
 
+    /**
+     * Sets Label-specific dimensions
+     * @param label label to set dimensions for
+     * @param dimensions dimensions values
+     */
     public static void setLabelDimensions(final Label label, final Dimensions dimensions) {
         label.relocate(dimensions.getX(), dimensions.getY());
         label.setMaxWidth(dimensions.getWidth());
@@ -179,6 +104,13 @@ public class ComponentUtils {
         label.setAlignment(Pos.CENTER);
     }
 
+
+    /**
+     * Sets Table-specific dimensions
+     * @param table table to set dimensions for
+     * @param dimensions dimensions values
+     * @param <D> Generic Type of TableView
+     */
     public static <D> void setTableDimensions(final TableView<D> table, final Dimensions dimensions) {
         table.relocate(dimensions.getX(), dimensions.getY());
         table.setMaxWidth(dimensions.getWidth());
